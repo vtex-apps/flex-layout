619c0242e90cf2191334b2d521a5e445499f00c0
Lucas Bebber <lucasbbebber@gmail.com>
2019-04-10 10:36:28 -0300

    Disallow using flex-layout directly

---
+++ react/Dummy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dummy = () => {
+  console.warn(
+    'You are inserting the `flex-layout` block directly. Please use `flex-layout.row` instead.'
+  )
+
+  return <span />
+}
+
+export default Dummy
