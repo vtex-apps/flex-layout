ce3dcc839608820183f2bc8f0c4c6ac1b4131207
Lucas Bebber <lucasbbebber@gmail.com>
2019-05-22 19:55:34 -0300

    Create distributedWidth util

---
+++ react/hooks/distributedWidth.ts
@@ -0,0 +1,117 @@
+import React, { ReactElement } from 'react'
+import { useMedia } from 'use-media'
+import { useRuntime } from 'vtex.render-runtime'
+import { parseWidth } from '../modules/valuesParser'
+
+interface DistributedWidthOptions {
+  preserveLayoutOnMobile?: boolean
+}
+
+interface ColWidth {
+  col: React.ReactNode
+  width: number
+  hasDefinedWidth: boolean
+  isResponsive: boolean
+}
+
+export const useResponsiveWidth = (
+  children: React.ReactNode,
+  options: DistributedWidthOptions
+) => {
+  const {
+    hints: { desktop },
+  } = useRuntime()
+
+  const isDesktop = useMedia({ minWidth: '40rem' }, desktop)
+  const isMobile = !isDesktop
+
+  const { preserveLayoutOnMobile = false } = options || {}
+
+  const cols: ColWidth[] = React.Children.toArray(children).map(col => {
+    const width = parseWidth((col as ReactElement).props.width)
+
+    if (width && typeof width === 'object') {
+      return {
+        col,
+        width: isMobile ? width.mobile || 0 : width.desktop || 0,
+        hasDefinedWidth: true,
+        isResponsive: true,
+      }
+    }
+
+    if (!preserveLayoutOnMobile && isMobile) {
+      return {
+        col,
+        width: 0,
+        hasDefinedWidth: false,
+        isResponsive: false,
+      }
+    }
+
+    if (typeof width === 'number') {
+      return {
+        col,
+        width,
+        hasDefinedWidth: true,
+        isResponsive: false,
+      }
+    }
+
+    return {
+      col,
+      width: 0,
+      hasDefinedWidth: false,
+      isResponsive: true,
+    }
+  })
+
+  const isAnyColResponsive = cols.some(col => col.isResponsive)
+  const breakOnMobile =
+    !preserveLayoutOnMobile && isMobile && !isAnyColResponsive
+
+  return {
+    cols,
+    breakOnMobile,
+  }
+}
+
+/** Distributes the available width--width that remains after subtracting
+ * the widths of the columns the user has set--between the columns that do
+ * not have their widths set by the user.
+ *
+ * For example:
+ * There are 3 columns. The user sets the width for the first one to 50%.
+ * This function will set the widths of the second and third columns to 25%.
+ */
+export const distributeAvailableWidth = (cols: ColWidth[]) => {
+  const { availableWidth, remainingColsNum } = cols.reduce(
+    (acc, col) => ({
+      availableWidth: acc.availableWidth - col.width,
+      remainingColsNum: acc.remainingColsNum + (col.hasDefinedWidth ? 0 : 1),
+    }),
+    {
+      availableWidth: 100,
+      remainingColsNum: 0,
+    }
+  )
+
+  if (availableWidth < 0) {
+    console.warn(
+      'Total width set for columns of a flex-layout.row block exceeds 100%.'
+    )
+    const normalization = -(100 / availableWidth)
+    cols = cols.map(col => ({
+      ...col,
+      width: col.width * normalization,
+    }))
+  }
+
+  return cols.map(col => ({
+    col: col.col,
+    width: `${
+      col.hasDefinedWidth
+        ? col.width
+        : Math.floor(Math.max(0, availableWidth) / remainingColsNum)
+    }%`,
+  }))
+}
