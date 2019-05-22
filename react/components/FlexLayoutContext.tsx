d4c4fcb703cada31a8a09e3be42e8894642136ce
Lucas Bebber <lucasbbebber@gmail.com>
2019-05-22 20:02:40 -0300

    Avoid exporting FlexLayoutContext, export Provider instead, and include rowGap

--- react/components/FlexLayoutContext.tsx
+++ react/components/FlexLayoutContext.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FunctionComponent, useContext } from 'react'
 
 export enum FlexLayoutTypes {
   NONE = 'none',
@@ -6,12 +6,38 @@ export enum FlexLayoutTypes {
   COL = 'col',
 }
 
-interface FlexLayoutContext {
+export interface FlexLayoutContextProps {
   parent: FlexLayoutTypes
   colGap: number
+  rowGap: number
 }
 
-export const FlexLayoutContext = React.createContext<FlexLayoutContext>({
+export type FlexLayoutContextValue = number | FlexLayoutTypes
+
+export type FlexLayoutContext = FlexLayoutContextProps & {
+  [key: string]: FlexLayoutContextValue
+}
+
+const FlexLayoutContext = React.createContext<FlexLayoutContext>({
   parent: FlexLayoutTypes.NONE,
   colGap: 0,
+  rowGap: 0,
 })
+
+export const FlexLayoutContextProvider: FunctionComponent<
+  FlexLayoutContextProps
+> = ({ parent, colGap, rowGap, children }) => {
+  return (
+    <FlexLayoutContext.Provider
+      value={{
+        parent,
+        colGap,
+        rowGap,
+      }}
+    >
+      {children}
+    </FlexLayoutContext.Provider>
+  )
+}
+
+export const useFlexLayoutContext = () => useContext(FlexLayoutContext)
